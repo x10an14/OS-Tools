15fe32d113f3afd077777f21bc3230b58212e958
x10an14 <x10an14@hotmail.com>
2016-07-15 12:21:52 +0200

    Ubuntu 15.10 home machine pre-reformat git commit of autossh/sshtunnel files.

--- linux/autossh/install_sshtunnel_systemd_service.sh
+++ linux/autossh/install_sshtunnel_systemd_service.sh
@@ -7,12 +7,12 @@ echo "$p1"
 echo "$p2"
 
 
-FILE=sshtunnel.session
+FILE=sshtunnel.service
 FOLDERPATH=$(dirname $(readlink -f $0))
 TEST="${FOLDERPATH}/${FILE}"
 echo "$FILE"
 if [[ -e $TEST ]]; then
-	#sudo systemctl enable $TEST
+	sudo systemctl enable $TEST
 	echo "yay!"
 else
 	echo "Did not find '$FILE' in the folder of this script!"
